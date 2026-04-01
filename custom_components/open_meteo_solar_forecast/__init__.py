6a0704957f7752361d949994370a96674b9407d8
Carleeno <54586926+carleeno@users.noreply.github.com>
2026-04-01 11:49:18 -0700

    Add service call to update array location to home location (#82)
    * Add service call to update array location to home location
    
    * Add optional location override field, move strings to translation files

--- custom_components/open_meteo_solar_forecast/__init__.py
+++ custom_components/open_meteo_solar_forecast/__init__.py
@@ -7,7 +7,7 @@ from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall
 
 from .const import (
     CONF_AZIMUTH,
@@ -130,6 +130,21 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+
+    async def async_update_array_location(call: ServiceCall | None = None):
+        new_location = call.data.get("location_override", {
+                "latitude": hass.config.latitude,
+                "longitude": hass.config.longitude,
+        })  # Optional location override defaults to current Home Assistant location
+
+        # Updating config entry will automatically update the coordinator
+        hass.config_entries.async_update_entry(
+            entry,
+            data={**entry.data, CONF_LATITUDE: new_location["latitude"], CONF_LONGITUDE: new_location["longitude"]},
+            options={**entry.options, CONF_LATITUDE: new_location["latitude"], CONF_LONGITUDE: new_location["longitude"]},
+        )
+
+    hass.services.async_register(DOMAIN, "update_array_location", async_update_array_location)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
